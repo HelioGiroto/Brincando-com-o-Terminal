dcca9df4bbcaae352a0b6bb482d2c82aec91a8eb
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-12-21 09:33:16 -0200

    Update detecta.sh

--- detecta.sh
+++ detecta.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-sleep 2
+sleep 3
 cd /home/h/Descargas/detecta
 fswebcam -bqr 320x240 --no-banner --png --save 0.png
 
@@ -15,8 +14,10 @@ do
 	#diff $foto_atual $foto_ant > /dev/null
 	
 	tolerancia=$(compare -fuzz 50% -metric ae $foto_atual $foto_ant null: 2>&1)
-	[ $tolerancia -eq 0 ] || cvlc --play-and-exit beep.mp3  		
+	[ $tolerancia -eq 0 ] || cvlc --play-and-exit beep.mp3
 
+	# cp $foto_atual DETECT-$foto_atual  # No caso de IF em vez de ternário acrescente essa linha		
+	# Acima SE tolerancia NAO é 0 - Pq tem os pipes || 
 	# [ $? -eq 0 ] && echo '' || cvlc --play-and-exit beep.mp3  
 	sufixo=$((sufixo+1))
 done
