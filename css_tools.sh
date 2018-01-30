74b93c98db158197af4984d1fcf2f437248b522b
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-01-30 01:38:04 -0200

    Update css_tools.sh

--- css_tools.sh
+++ css_tools.sh
@@ -4,8 +4,6 @@ echo; sed 's/>/>\n\n\t/; s/{/ {\n\t\t/g; s/}/\n\t}\n\n\t/g; s/;/;\n\t\t/g; s|\t<
 # cat s.css | tr '\n' ' ' | sed -r 's/\t//g; s/  //g' ## Transforma CSS em CSS MINIMAL !!!
 
 
-
-
 # <style type="text/css">body{background:tomato;text-align:center}.centro{position:relative;width:50%;left:25%}.images{display:flex;flex-flow:column nowrap;justify-content:center}.images>div{width:auto;height:auto;margin:15px 0;justify-content:center}.foto{}.ficha{}.obs{width:50%;padding:30px 0;background:#acf5ff;margin-bottom:25px}.botoes{display:flex;flex-flow:row nowrap;justify-content:space-around}.botoes>div{background:#c1c1c1;padding:7px 20px;font-size:2em}</style>
 
 # Criar filtro externo para BlueFish para dar um pretty css na linha acima:
