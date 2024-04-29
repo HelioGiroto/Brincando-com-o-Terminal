fd5553154e7441dcbb386ce012790f114dd4ca2a
helio <helio@i3>
2024-04-29 10:47:57 -0300

    29/04

--- Debian Minimal + Openbox/2-Pacotes-essenciais.md
+++ Debian Minimal + Openbox/2-Pacotes-essenciais.md
@@ -53,8 +53,10 @@ startx
 - Pacotes essenciais: 
 
 ```bash
-sudo apt install vim tmux ranger gedit chromium simplescreenrecorder audacity gcc cheese conky bc gawk sed grep lynx curl wget html-xml-utils xclip googler ffmpeg imagemagick speedtest-cli youtube-dl nmap numix-icon-theme-circle espeak mbrola gnumeric galculator abiword shotwell htop mc mupdf mpack ssmtp mplayer alpine wkhtmltopdf gpick gnome-tweaks gnome-sushi gnome-multi-writer wdiff colordiff tint2 feh fswebcam dialog jq xtrlock mate-utils pavucontrol calendar whois bsdmainutils onboard pdfgrep numlockx lxappearance vlc python3-pip mirage plank xserver-xorg-input-synaptics nodejs npm pandoc gnome-control-center mediainfo mp3info mpv openjdk-18-jre default-jre xdotool youtube-dl neofetch
+sudo apt install vim tmux ranger gedit chromium simplescreenrecorder audacity gcc cheese conky bc gawk sed grep lynx curl wget html-xml-utils xclip googler ffmpeg imagemagick speedtest-cli youtube-dl nmap numix-icon-theme-circle espeak mbrola gnumeric galculator abiword shotwell htop mc mupdf mpack ssmtp mplayer alpine wkhtmltopdf gpick gnome-tweaks gnome-sushi gnome-multi-writer wdiff colordiff tint2 feh fswebcam dialog jq xtrlock mate-utils pavucontrol calendar whois bsdmainutils onboard pdfgrep numlockx lxappearance vlc python3-pip mirage plank xserver-xorg-input-synaptics nodejs npm pandoc gnome-control-center mediainfo mp3info mpv xdotool youtube-dl neofetch default-jre
 ``
+
+# openjdk-18-jre    # JDonwloader: não aceita instalar via apt
 
 Pronto!
 
--- Debian Minimal + Openbox/2-download-conf-ob.sh
+++ Debian Minimal + Openbox/2-download-conf-ob.sh
@@ -7,24 +7,54 @@
 
 # Baixa arquivos necessários de Github para a máquina (na pasta correta) onde o Openbox está sendo instalado:
 
+# Clona repositório para a pasta da máquina que será instalada:
+git clone ...
+
+# Abre a pastado repositório clonado para a máquina:
+cd Brincando-com-o-Termianl
+cd Debian\ Minimal\ +\ Openbox
+cd arquivos-de-configuracao
+cd openbox
+
 # openbox
-cd ~/.config/openbox
+cp * ~/.config/openbox
+# Arquivos copiados para ~/.config/openbox da máquina:
 	# rc.xml 	- responsável por teclas de atalho
 	# menu.xml	- responsável por configurar o menu
 	# autostart	- script de inicialização
-# wget 
 
 
-# Tint2
-cd ~/.config/tint2
-# falta colocar o tint.conf correto = ???
-# wget
+
+# lxterminal
+# volta uma pasta do repositório clonado. Abre lxterminal e copia para a máquina:
+cd ..
+cd lxterminal
+cp * ~/.config/lxterminal
+# Arquivos copiados para: ~/.config/lxterminal
+# lxterminal.conf	- arquivo de configuração para o lxterminal
 
 
 # conky
-cd /etc/conky
-# dois arquivos de configuração para o conky personalizado
-# wget
+# volta uma pasta do repositório clonado. Abre conky e copia tudo para a máquina:
+cd ..
+cd conky
+sudo cp * /etc/conky
+
+
+# Thunar:
+# volta uma pasta do repositório clonado. Abre Thunar e copia para a máquina:
+cd ..
+cd thunar
+cp * ~/.config/Thunar
+
+
+# Tint2
+# volta uma pasta do repositório clonado. Abre tint2 e copia para a máquina:
+cd ..
+cd tint2
+cp * ~/.config/tint2
+# falta colocar o tint.conf correto = ???
+
 
 
 # rofi
@@ -32,15 +62,6 @@ cd ~/.config/rofi
 # arquivo de configuração do rofi
 # wget
 
-# lxterminal
-cd ~/.config/lxterminal
-# lxterminal.conf	- arquivo de configuração para o lxterminal
-# wget .....
-
-# Thunar:
-cd ~/.config/Thunar
-# todos os arquivos:
-# wget .....
 
 
 # fontes
