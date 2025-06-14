f00712aa5fe1ac6959f04a7e02e9d95f3f71d644
H <h@pc>
2025-06-13 22:54:01 -0300

    edição de openbox para o pc-linux-gamer

---
+++ Debian Minimal + Openbox/script_instalador.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# instalador de arquivos de configuração em debian minimal + openbox 
+
+# openbox 
+
+sudo mkdir ~/.config/openbox
+sudo cd ~/.config/openbox
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/openbox/autostart
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/openbox/menu.xml
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/openbox/mudar%20rc_xml.md
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/openbox/rc.xml
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/openbox/old_rc.xml
+
+# baixa para pasta conky:
+sudo cd /etc/conky
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/conky/conky_info
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/conky/conky_shortcuts
+
+ 
+# lxterminal
+sudo cd ~/.config/lxterminal
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/lxterminal/lxterminal.conf
+
+
+
+# rofi
+sudo cd ~/.config/rofi
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/rofi/config.rasi
+
+
+# thunar
+sudo cd ~/.config/Thunar
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/thunar/accels.scm
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/thunar/readme.md
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/thunar/thunar-config.md
+
+sudo wget https://raw.githubusercontent.com/HelioGiroto/Brincando-com-o-Terminal/refs/heads/master/Debian%20Minimal%20%2B%20Openbox/arquivos_de_configuracao/thunar/uca.xml
+
+
+# 	
+
+
