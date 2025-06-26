201510177bd49792fcf3658e8f0ca74fdb2c4315
H <h@pc>
2025-06-25 22:30:53 -0300

    Scritps de instalação 25/06/2025

---
+++ Debian Minimal + Openbox/screen_conf_linux_pcgamer.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output DVI-1 --off --output VGA-1-1 --mode 1600x900 --pos 1920x0 --rotate normal --output HDMI-1-1 --off
